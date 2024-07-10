8f4a59a53d7783c531bef7f5c761a4f5d13503fc
Arslan <arslanazhar00@gmail.com>
2024-07-10 21:01:22 +0500

    removed unecessary code

--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-// Import necessary styles and libraries
-// import './assets/main.css';  // Example: Import your main CSS file
-// import 'primeicons/primeicons.css';  // Example: Import PrimeIcons CSS
-// import Toast from 'vue-toastification';  // Example: Import Vue Toastification library
-// import 'vue-toastification/dist/index.css';  // Example: Import Vue Toastification styles
 import ToastPlugin from "vue-toast-notification";
 import "vue-toast-notification/dist/theme-bootstrap.css";
 import router from "./router";
